20615fe51208412d6113247c31404da4a87e359d
Griffith <griffithtp@gmail.com>
2021-02-28 21:44:45 +0000

    add ItemHandlerTest

---
+++ src/test/java/com/learnreactivespring/learnreactivespring/handler/ItemHandlerTest.java
@@ -0,0 +1,93 @@
+package com.learnreactivespring.learnreactivespring.handler;
+
+
+import com.learnreactivespring.learnreactivespring.document.Item;
+import com.learnreactivespring.learnreactivespring.repository.ItemReactiveRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.learnreactivespring.learnreactivespring.constants.ItemConstants.ITEM_END_POINT_V1;
+import static com.learnreactivespring.learnreactivespring.constants.ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1;
+
+@SpringBootTest
+@DirtiesContext
+@AutoConfigureWebTestClient
+@ActiveProfiles("test")
+public class ItemHandlerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    public List<Item> data() {
+
+        return Arrays.asList(new Item(null, "Samsung TV", 399.99)
+                , new Item(null, "LG TV", 399.99)
+                , new Item(null, "Apple Watch", 349.99)
+                , new Item(null, "Apple TV", 199.99)
+                , new Item("ABC", "Apple Mac Mini", 999.00));
+    }
+
+    @BeforeEach
+    public void setup() {
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(data()))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext(item -> System.out.println("Inserted item : " + item))
+                .blockLast();
+    }
+
+    @Test
+    public void getAllItems() {
+        webTestClient.get().uri(ITEM_FUNCTIONAL_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Item.class)
+                .hasSize(5);
+    }
+
+    @Test
+    public void getAllItems_approach2() {
+        webTestClient.get().uri(ITEM_FUNCTIONAL_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Item.class)
+                .hasSize(5)
+                .consumeWith(response -> {
+                    List<Item> items = response.getResponseBody();
+                    items.forEach(item -> {
+                        assert(item.getId() != null);
+                    });
+                });
+    }
+
+    @Test
+    public void getAllItems_approach3() {
+        Flux<Item> itemFlux = webTestClient.get().uri(ITEM_FUNCTIONAL_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .returnResult(Item.class)
+                .getResponseBody();
+
+        StepVerifier.create(itemFlux.log("inserted: "))
+                .expectNextCount(5)
+                .verifyComplete();
+    }
+}
